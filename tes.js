f20a858ad56c7ff292757a555f6765061a4a247b
lunairaa <acawapi@gmail.com>
2025-06-23 15:46:37 +0700

    Update tes.js

--- tes.js
+++ tes.js
@@ -1,5 +1,5 @@
 export default function (app) {
   app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello from raw route!' })
+    res.json({ message: 'Hello from raw route!.....' })
   })
 }
